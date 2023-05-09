3e09b71aab4380f49878ed50511e82e4b72ec557
mnath12 <moinak.nath@yahoo.com>
2023-05-09 13:13:52 -0500

    Streamline benchmarking functions

--- Spring2023.py
+++ Spring2023.py
@@ -1,4 +1,4 @@
-# ## Writing Functions to Facilitate Benchmarking
+# # # Writing Functions to Facilitate Benchmarking
 import scqubits as scq
 import numpy as np
 import scipy as sp
@@ -30,7 +30,7 @@ def scipy_option_tester(qubit, dims, N):
         end_time_2 = time()
         times_2[i] = (end_time_2-start_time_2)/N
         times_ratio[i] = times_2[i]/times_1[i]
-        print(f"Time ratio (SA/LM) {times[i]:.2f} for {dim:.0f}x{dim:.0f}")
+        print(f"Time ratio (SA/LM) {times_ratio[i]:.2f} for {dim:.0f}x{dim:.0f}")
     return times_ratio
 
 def generate_plot(times, dims, x_label, y_label, title, file_name):
@@ -42,28 +42,40 @@ def generate_plot(times, dims, x_label, y_label, title, file_name):
     plt.show()
     return None
 
-def primme_benchmarker(qubit, shape, N, energies, ncuts):
+def primme_benchmarker(qubit, shape, avg_num, param_1, a, b, param_2, c, d, file_name):
     # shape: tuple with size of matrix (m,n)
-    # N: averaging
+    # avg_num: number of iterations for averaging
+    # param_1: first parameter to be varied
+    # a: cutoff for p1
+    # b: cutoff for p1
+    # param_2:
+    # c: cutoff
+    # d: cutoff
+    # file_name:
     times_a = np.empty(shape = shape)
     times_b = np.empty(shape = shape)
-    for index_1, energy in tqdm(enumerate(energies)):
-        qubit.EJ = energy
-        for index_2, dim in enumerate(ncuts):
-            qubit.ncut = dim
+    param_1_list = np.linspace(a, b, shape[0])
+    param_2_list = np.linspace(c, d, shape[1])
+    for index_1, energy in tqdm(enumerate(param_1_list)):
+        qubit.param_1 = energy
+        for index_2, p in enumerate(param_2):
+            qubit.param_2 = p
+            #for ncut in qubit.cutoff_names:
+                #qubit.__dict__["_" + ncut] = dim
             hamiltonian = qubit.hamiltonian()
             start_time_1 = time()
-            for _ in range(N):
+            for _ in range(avg_num):
                 result = pr.eigsh(hamiltonian, k=6, sigma = 0.0, which="LM")
             end_time_1 = time()
-            times_a[index_1][index_2] = (end_time_1-start_time_1)/N
+            times_a[index_1][index_2] = (end_time_1-start_time_1)/avg_num
             start_time = time()
-            for _ in range(N):
+            for _ in range(avg_num):
                 result = sp.sparse.linalg.eigsh(hamiltonian, k=6, sigma = 0.0, which="LM")
             end_time = time()
-            times_b[index_1][index_2] = (end_time-start_time)/N
-    times_ratio = times_a / times_b        
-    return times_ratio         
+            times_b[index_1][index_2] = (end_time-start_time)/avg_num
+    times_ratio = times_a / times_b    
+    np.save(file_name, times_ratio)    
+    return None         
 
 
 # Generates a heatmap of PRIMME performance given an appropriate matrix
